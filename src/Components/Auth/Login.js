69f6993485ba3cdf4df2d580601997d98e2bd164
soufiane odf <soufiane.odf@gmail.com>
2020-05-07 03:58:15 +0000

    Update Login.js
    Add firebase logic to login page

--- src/Components/Auth/Login.js
+++ src/Components/Auth/Login.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Input from '../../utils/forms/Input';
 import CustomButton from '../../utils/forms/CustomButton';
 import {View, Text, StyleSheet, TouchableWithoutFeedback} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-
+import { firebaseAuth } from '../../environment/config';
 
 const Login = (props) => {
+
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [errorMessage, setErrorMessage] = useState("");
 
 	function _loginMethodButton(name, bgColor, text) {
 		return(
@@ -23,15 +27,37 @@ const Login = (props) => {
 		);
 	}
 
+	function _handleLogin() {
+    firebaseAuth.signInWithEmailAndPassword(email, password).then(() => { props.navigation.navigate('Main'); })
+    .catch(error => setErrorMessage(error.message) );
+	}
+
 	return (
 		<View style={styles.viewContainer}>
+			{errorMessage ? (<Text>{errorMessage}</Text>) : null}
 			{_loginMethodButton("logo-facebook", "#39589A", "Login with Facebook")}
 			{_loginMethodButton("logo-twitter", "#50ABF1", "Login with Twitter")}
 			{_loginMethodButton("logo-google", "#DD4B39", "Login with Google")}
 
-			<Input placeholder={"Email"} keyboardType={"email-address"} iconType={"Zocial"} iconName={"email"} iconSize={18} />
-			<Input placeholder={"Password"} iconType={"Ionicons"} iconName={"ios-lock"} iconSize={18} showOrHidePassword={true} />
-			<CustomButton title={"LOG IN"} color={"#2db7ff"} />
+			<Input 
+				placeholder={"Email"} 
+				keyboardType={"email-address"} 
+				iconType={"Zocial"} 
+				iconName={"email"} 
+				iconSize={18}    
+				onChangeText={email => {setEmail(email); setErrorMessage("")}} 
+				action={_handleLogin} 
+			/>
+			<Input 
+				placeholder={"Password"} 
+				iconType={"Ionicons"} 
+				iconName={"ios-lock"} 
+				iconSize={18} 
+				showOrHidePassword={true} 
+				onChangeText={password => {setPassword(password); setErrorMessage("")}} 
+				action={_handleLogin} 
+			/>
+			<CustomButton title={"LOG IN"} color={"#2db7ff"} action={_handleLogin} />
 			<View style={styles.bottomContainer}>
 				<TouchableWithoutFeedback onPress={() => props.navigation.navigate("ForgotPassword")}>
 					<Text style={styles.forgotPasswordText}>Forgot password?</Text>
