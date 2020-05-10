aab337a0ffab1fc0a6838491a08f5ceaa0b4ef34
soufiane odf <soufiane.odf@gmail.com>
2020-05-10 03:40:29 +0000

    Add Drawer Navigation

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import Login from '../Components/Auth/Login';
 import Signup from '../Components/Auth/Signup';
 import ForgotPassword from '../Components/Auth/ForgotPassword';
@@ -10,18 +11,28 @@ import Loading from '../Components/Auth/Loading';
 
 const Navigation = () => {
 
+  const Drawer = createDrawerNavigator();
+  const DrawerScreen = () => (
+      <Drawer.Navigator>
+        <Drawer.Screen name="Main" component={Main} />
+      </Drawer.Navigator>
+  );
+
   const Stack = createStackNavigator();
+  const App = () => (
+  <Stack.Navigator initialRouteName="Loading" screenOptions={{headerShown: false}}>
+    <Stack.Screen name="Login" component={Login} />
+    <Stack.Screen name="Signup" component={Signup} />
+    <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+    <Stack.Screen name="CheckEmail" component={CheckEmail} />
+    <Stack.Screen name="Loading" component={Loading} />
+    <Stack.Screen name="Main" component={DrawerScreen} />
+  </Stack.Navigator>
+  );
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Loading" screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Signup" component={Signup} />
-        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
-        <Stack.Screen name="CheckEmail" component={CheckEmail} />
-        <Stack.Screen name="Main" component={Main} />
-        <Stack.Screen name="Loading" component={Loading} />
-      </Stack.Navigator>
+      <App/>
     </NavigationContainer>
   );
 }
