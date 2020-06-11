62f819cb9e42f26ffe6f725688191ddea8115457
soufiane odf <soufiane.odf@gmail.com>
2020-06-11 20:00:13 +0100

    Upload and Get avatar

--- src/utils/Avatar.js
+++ src/utils/Avatar.js
@@ -1,21 +1,30 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {StyleSheet, Image, TouchableOpacity} from 'react-native';
 import storage from '@react-native-firebase/storage';
+import auth from '@react-native-firebase/auth';
 import ImagePicker from 'react-native-image-picker';
 
 const Avatar = () => {
-  const [url, setUrl] = useState('gs://react-native-medical-record.appspot.com/app-images/avatar.png');
-  const [avatar, setavatar] = useState('');
+  const [url, setUrl] = useState(
+    'gs://react-native-medical-record.appspot.com/app-images/avatar.png',
+  );
+  const [uuid, setUuid] = useState(null);
+  const isInitMounted = useRef(true);
+  const reference = storage().ref(`app-images/${uuid}/avatar.png`);
 
   useEffect(() => {
-    storage()
-      .ref('app-images/avatar.png')
-      .getDownloadURL()
-      .then(url => {
-        setUrl(url);
-      })
-      .catch(error => alert(error));
-  }, [url]);
+    if (isInitMounted.current) {
+      console.log('form if ');
+      isInitMounted.current = false;
+      auth().onAuthStateChanged(user => {
+        if (user) {
+          setUuid(user.uid);
+        }
+      });
+    }
+    console.log('form useEffect ' + uuid);
+    _getImage();
+  });
 
   function _avatarClicked() {
     ImagePicker.showImagePicker({}, response => {
@@ -25,17 +34,35 @@ const Avatar = () => {
         console.log('Erreur : ', response.error);
       } else {
         console.log('Photo : ', response.uri);
-        let requireSource = {uri: response.uri};
-        setavatar(requireSource);
+        _uploadImage(response.uri);
       }
     });
+  }
+
+  function _getImage() {
+    storage()
+      .ref(`app-images/${uuid}/avatar.png`)
+      .getDownloadURL()
+      .then(theurl => {
+        console.log('i am getting the image');
+        setUrl(theurl);
+      })
+      .catch(error => console.log(error));
+  }
+
+  async function _uploadImage(uri) {
+    // path to existing file on filesystem
+    const pathToFile = uri;
+    // uploads file
+    await reference.putFile(pathToFile);
+    _getImage();
   }
 
   return (
     <TouchableOpacity
       style={styles.touchableOpacity}
       onPress={() => _avatarClicked()}>
-      <Image style={styles.avatar} source={avatar} />
+      <Image style={styles.avatar} source={{uri: url}} />
     </TouchableOpacity>
   );
 };
