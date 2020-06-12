1fa695318dec40d30f28dd0b4b549492ca454e48
soufiane odf <soufiane.odf@gmail.com>
2020-06-12 21:50:06 +0100

    Add some unusefull test code

---
+++ Intro.js
@@ -0,0 +1,49 @@
+// Intro.js
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  instructions: {
+    color: '#333333',
+    marginBottom: 5,
+    textAlign: 'center',
+  },
+  welcome: {
+    fontSize: 20,
+    margin: 10,
+    textAlign: 'center',
+  },
+});
+
+export default class Intro extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: '',
+    };
+  }
+
+  _sum(x, y) {
+    // return x + y;
+    this.setState({
+      data: x + y,
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text testID="username" style={styles.instructions}>
+          This is a React Native snapshot test. And this is another line.
+        </Text>
+      </View>
+    );
+  }
+}
