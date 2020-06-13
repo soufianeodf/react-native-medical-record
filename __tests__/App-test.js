816600a0013adab450443768944eb74b448ca603
soufiane odf <soufiane.odf@gmail.com>
2020-06-13 21:46:23 +0100

    Add App-test.js

---
+++ __tests__/App-test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import App from '../App';
+import {shallow} from 'enzyme';
+import {findByTestAttr} from '../tests/testUtils';
+
+/**
+ * Factory function to create a ShallowWrapper for the App component
+ * @function setup
+ * @param {object} props - Component props specific to this step.
+ * @param {object} state - Initial state for setup.
+ * @returns {ShallowWrapper}
+ */
+const setup = (props = {}) => {
+  return shallow(<App {...props} />);
+};
+
+test('renders without error', () => {});
