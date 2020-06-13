3df6c290151407eee9e02f7d02541976269433f7
soufiane odf <soufiane.odf@gmail.com>
2020-06-13 21:44:34 +0100

    Add test setup

---
+++ tests/testUtils.js
@@ -0,0 +1,9 @@
+/**
+ * Return ShallowWrapper containing node(s) with the given data-test value.
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within
+ * @param {string} val - Value of data-test attribute for search
+ * @returns {ShallowWrapper}
+ */
+export const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+};
