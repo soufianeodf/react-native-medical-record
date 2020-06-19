1f0a83860a675357236094b543311e85bf896de9
soufiane odf <soufiane.odf@gmail.com>
2020-06-19 18:46:57 +0100

    Update the UI design of profile

--- src/Components/Screens/Profile/index.js
+++ src/Components/Screens/Profile/index.js
@@ -144,7 +144,9 @@ const Profile = ({navigation}) => {
   };
 
   return (
-    <ScrollView keyboardShouldPersistTaps="always">
+    <ScrollView
+      keyboardShouldPersistTaps="always"
+      style={styles.scrollViewContainer}>
       <View style={styles.headerContainer}>
         <View style={styles.iconCheck}>
           <TouchableOpacity onPress={_handleUpdate}>
@@ -163,227 +165,211 @@ const Profile = ({navigation}) => {
         <Text style={styles.headerText}>Profile information</Text>
       </View>
 
-      {successMessage ?
-        (<View style={{alignItems: 'center'}}>
-          <View
-            style={{position: 'absolute', top: '20%', left: '16%', zIndex: 5}}>
-            <Entypo name={'check'} size={20} />
+      <View style={styles.helperDesignView} />
+
+      <View style={styles.formContainer}>
+        {successMessage ? (
+          <View style={styles.successMessage}>
+            <View style={styles.successMessageIcon}>
+              <Entypo name={'check'} size={20} />
+            </View>
+            <Text style={styles.successTextMessage}>{successMessage}</Text>
           </View>
-          <Text
-            style={{
-              textAlign: 'center',
-              backgroundColor: '#4AB866',
-              alignItems: 'center',
-              padding: 8,
-              width: '92%',
-              borderRadius: 2,
-            }}>
-            {successMessage}
-          </Text>
-        </View>
-      ) : null}
+        ) : null}
 
-      {errorMessage ?
-        (<View style={{alignItems: 'center'}}>
-          <View
-            style={{position: 'absolute', top: '21%', left: '7%', zIndex: 5}}>
-            <FontAwesome name={'exclamation-circle'} size={20} />
+        {errorMessage ? (
+          <View style={styles.successMessage}>
+            <View style={styles.errorMessageIcon}>
+              <FontAwesome name={'exclamation-circle'} size={20} />
+            </View>
+            <Text style={styles.errorTextMessage}>{errorMessage}</Text>
           </View>
-          <Text
-            style={{
-              textAlign: 'center',
-              backgroundColor: '#D94F4F',
-              alignItems: 'center',
-              padding: 8,
-              width: '92%',
-              borderRadius: 2,
-            }}>
-            {errorMessage}
-          </Text>
-        </View>
-      ) : null}
+        ) : null}
 
-      {_clearMessage()}
+        {_clearMessage()}
 
-      <Text style={styles.cardTitle}>Main info</Text>
-      <View style={styles.card}>
-        <View style={styles.cardContent}>
-          <TextInput
-            style={styles.textInput}
-            onChangeText={username => setUsername(username)}
-            placeholder={'Username'}
-            value={username}
-            onSubmitEditing={_handleUpdate}
-          />
-          <TextInput
-            style={styles.textInput}
-            onChangeText={fullName => setFullName_card_1(fullName)}
-            placeholder={'Full Name'}
-            value={fullName_card_1}
-            onSubmitEditing={_handleUpdate}
-          />
-
-          <TouchableOpacity
-            style={styles.touchableOpacityDatePicker}
-            onPress={_showDatePicker}>
+        <Text style={styles.cardTitle}>Main info</Text>
+        <View style={styles.card}>
+          <View style={styles.cardContent}>
             <TextInput
-              style={[styles.textInput, {width: '96%'}]}
-              editable={false}
-              placeholder={'Date of birth'}
-              value={birthDate}
+              style={styles.textInput}
+              onChangeText={theUsername => setUsername(theUsername)}
+              placeholder={'Username'}
+              value={username}
+              onSubmitEditing={_handleUpdate}
             />
-          </TouchableOpacity>
+            <TextInput
+              style={styles.textInput}
+              onChangeText={fullName => setFullName_card_1(fullName)}
+              placeholder={'Full Name'}
+              value={fullName_card_1}
+              onSubmitEditing={_handleUpdate}
+            />
 
-          <TextInput
-            style={styles.textInput}
-            placeholder={'Gender'}
-            onTouchEnd={() => {
-              setIsGenderVisible(true);
-              Keyboard.dismiss();
-            }}
-            value={gender}
-            caretHidden={true}
-          />
-          <TextInput
-            style={styles.textInput}
-            editable={false}
-            placeholder={'Email'}
-            value={email}
-          />
-          <TextInput
-            style={styles.textInput}
-            onChangeText={phone => setPhone(phone)}
-            placeholder={'Phone'}
-            keyboardType={'phone-pad'}
-            value={phone}
-            onSubmitEditing={_handleUpdate}
-          />
+            <TouchableOpacity
+              style={styles.touchableOpacityDatePicker}
+              onPress={_showDatePicker}>
+              <TextInput
+                style={[styles.textInput, {width: '96%'}]}
+                editable={false}
+                placeholder={'Date of birth'}
+                value={birthDate}
+              />
+            </TouchableOpacity>
+
+            <TextInput
+              style={styles.textInput}
+              placeholder={'Gender'}
+              onTouchEnd={() => {
+                setIsGenderVisible(true);
+                Keyboard.dismiss();
+              }}
+              value={gender}
+              caretHidden={true}
+            />
+            <TextInput
+              style={styles.textInput}
+              editable={false}
+              placeholder={'Email'}
+              value={email}
+            />
+            <TextInput
+              style={styles.textInput}
+              onChangeText={thePhone => setPhone(thePhone)}
+              placeholder={'Phone'}
+              keyboardType={'phone-pad'}
+              value={phone}
+              onSubmitEditing={_handleUpdate}
+            />
+          </View>
         </View>
-      </View>
 
-      <Text style={styles.cardTitle}>Health insurance</Text>
-      <View style={styles.card}>
-        <View style={styles.cardContent}>
-          <TextInput
-            style={styles.textInput}
-            onChangeText={fullName => setFullName_card_2(fullName)}
-            placeholder={'Full Name'}
-            value={fullName_card_2}
-            onSubmitEditing={_handleUpdate}
-          />
-          <TextInput
-            style={styles.textInput}
-            onChangeText={affiliationNumber =>
-              setAffiliationNumber(affiliationNumber)
-            }
-            placeholder={'Affiliation number'}
-            keyboardType={'numeric'}
-            value={affiliationNumber}
-            onSubmitEditing={_handleUpdate}
-          />
-          <TextInput
-            style={styles.textInput}
-            onChangeText={registrationNumber =>
-              setRegistrationNumber(registrationNumber)
-            }
-            placeholder={'Registration number'}
-            keyboardType={'numeric'}
-            value={registrationNumber}
-            onSubmitEditing={_handleUpdate}
-          />
-          <TextInput
-            style={styles.textInput}
-            onChangeText={cin => setCin(cin)}
-            placeholder={'CIN'}
-            value={cin}
-            onSubmitEditing={_handleUpdate}
-          />
-          <TextInput
-            style={[styles.textInput, {width: '100%'}]}
-            placeholder={'Relationship of the beneficiary to the insured'}
-            onTouchEnd={() => {
-              setIsRelationshipVisible(true);
-              Keyboard.dismiss();
-            }}
-            value={relationship}
-            caretHidden={true}
-          />
-          <TextInput
-            style={[styles.textInput, {width: '100%'}]}
-            onChangeText={address => setAddress(address)}
-            placeholder={'Address'}
-            value={address}
-            onSubmitEditing={_handleUpdate}
-          />
-          <TextInput
-            style={styles.textInput}
-            onChangeText={amoutOfFees => setAmoutOfFees(amoutOfFees)}
-            placeholder={'Amount of fees (Dhs)'}
-            keyboardType={'decimal-pad'}
-            value={amoutOfFees}
-            onSubmitEditing={_handleUpdate}
-          />
-          <TextInput
-            style={styles.textInput}
-            onChangeText={attachmentNumber =>
-              setAttachmentNumber(attachmentNumber)
-            }
-            placeholder={'Number of attachments'}
-            keyboardType={'numeric'}
-            value={attachmentNumber}
-            onSubmitEditing={_handleUpdate}
-          />
+        <Text style={styles.cardTitle}>Health insurance</Text>
+        <View style={styles.card}>
+          <View style={styles.cardContent}>
+            <TextInput
+              style={styles.textInput}
+              onChangeText={fullName => setFullName_card_2(fullName)}
+              placeholder={'Full Name'}
+              value={fullName_card_2}
+              onSubmitEditing={_handleUpdate}
+            />
+            <TextInput
+              style={styles.textInput}
+              onChangeText={theAffiliationNumber =>
+                setAffiliationNumber(theAffiliationNumber)
+              }
+              placeholder={'Affiliation number'}
+              keyboardType={'numeric'}
+              value={affiliationNumber}
+              onSubmitEditing={_handleUpdate}
+            />
+            <TextInput
+              style={styles.textInput}
+              onChangeText={theRegistrationNumber =>
+                setRegistrationNumber(theRegistrationNumber)
+              }
+              placeholder={'Registration number'}
+              keyboardType={'numeric'}
+              value={registrationNumber}
+              onSubmitEditing={_handleUpdate}
+            />
+            <TextInput
+              style={styles.textInput}
+              onChangeText={theCin => setCin(theCin)}
+              placeholder={'CIN'}
+              value={cin}
+              onSubmitEditing={_handleUpdate}
+            />
+            <TextInput
+              style={[styles.textInput, {width: '100%'}]}
+              placeholder={'Relationship of the beneficiary to the insured'}
+              onTouchEnd={() => {
+                setIsRelationshipVisible(true);
+                Keyboard.dismiss();
+              }}
+              value={relationship}
+              caretHidden={true}
+            />
+            <TextInput
+              style={[styles.textInput, {width: '100%'}]}
+              onChangeText={theAddress => setAddress(theAddress)}
+              placeholder={'Address'}
+              value={address}
+              onSubmitEditing={_handleUpdate}
+            />
+            <TextInput
+              style={styles.textInput}
+              onChangeText={theAmoutOfFees => setAmoutOfFees(theAmoutOfFees)}
+              placeholder={'Amount of fees (Dhs)'}
+              keyboardType={'decimal-pad'}
+              value={amoutOfFees}
+              onSubmitEditing={_handleUpdate}
+            />
+            <TextInput
+              style={styles.textInput}
+              onChangeText={theAttachmentNumber =>
+                setAttachmentNumber(theAttachmentNumber)
+              }
+              placeholder={'Number of attachments'}
+              keyboardType={'numeric'}
+              value={attachmentNumber}
+              onSubmitEditing={_handleUpdate}
+            />
+          </View>
         </View>
+
+        <CustomModal
+          isItVisible={isGenderVisible}
+          _onRequestClose={() => setIsGenderVisible(!isGenderVisible)}
+          title={'Gender'}
+          _onPress_1={() => {
+            setGender('MALE');
+            setIsGenderVisible(!isGenderVisible);
+          }}
+          variable_1={'MALE'}
+          _onPress_2={() => {
+            setGender('FEMALE');
+            setIsGenderVisible(!isGenderVisible);
+          }}
+          variable_2={'FEMALE'}
+          _onPress_3={() => {
+            setGender('');
+            setIsGenderVisible(!isGenderVisible);
+          }}
+          variable_3={"I'M NOT SURE"}
+        />
+
+        <CustomModal
+          isItVisible={isRelationshipVisible}
+          _onRequestClose={() =>
+            setIsRelationshipVisible(!isRelationshipVisible)
+          }
+          title={'Relationship of the beneficiary'}
+          _onPress_1={() => {
+            setRelationship('CHILD');
+            setIsRelationshipVisible(!isRelationshipVisible);
+          }}
+          variable_1={'CHILD'}
+          _onPress_2={() => {
+            setRelationship('SPOUSE');
+            setIsRelationshipVisible(!isRelationshipVisible);
+          }}
+          variable_2={'SPOUSE'}
+          _onPress_3={() => {
+            setRelationship('');
+            setIsRelationshipVisible(!isRelationshipVisible);
+          }}
+          variable_3={"I'M NOT SURE"}
+        />
+
+        <DateTimePickerModal
+          isVisible={isDatePickerVisible}
+          mode="date"
+          onConfirm={_handleConfirm}
+          onCancel={_hideDatePicker}
+        />
       </View>
-
-      <CustomModal
-        isItVisible={isGenderVisible}
-        _onRequestClose={() => setIsGenderVisible(!isGenderVisible)}
-        title={'Gender'}
-        _onPress_1={() => {
-          setGender('MALE');
-          setIsGenderVisible(!isGenderVisible);
-        }}
-        variable_1={'MALE'}
-        _onPress_2={() => {
-          setGender('FEMALE');
-          setIsGenderVisible(!isGenderVisible);
-        }}
-        variable_2={'FEMALE'}
-        _onPress_3={() => {
-          setGender('');
-          setIsGenderVisible(!isGenderVisible);
-        }}
-        variable_3={"I'M NOT SURE"}
-      />
-
-      <CustomModal
-        isItVisible={isRelationshipVisible}
-        _onRequestClose={() => setIsRelationshipVisible(!isRelationshipVisible)}
-        title={'Relationship of the beneficiary'}
-        _onPress_1={() => {
-          setRelationship('CHILD');
-          setIsRelationshipVisible(!isRelationshipVisible);
-        }}
-        variable_1={'CHILD'}
-        _onPress_2={() => {
-          setRelationship('SPOUSE');
-          setIsRelationshipVisible(!isRelationshipVisible);
-        }}
-        variable_2={'SPOUSE'}
-        _onPress_3={() => {
-          setRelationship('');
-          setIsRelationshipVisible(!isRelationshipVisible);
-        }}
-        variable_3={"I'M NOT SURE"}
-      />
-
-      <DateTimePickerModal
-        isVisible={isDatePickerVisible}
-        mode="date"
-        onConfirm={_handleConfirm}
-        onCancel={_hideDatePicker}
-      />
     </ScrollView>
   );
 };
@@ -391,12 +377,32 @@ const Profile = ({navigation}) => {
 export default Profile;
 
 const styles = StyleSheet.create({
+  scrollViewContainer: {
+    flex: 1,
+    backgroundColor: '#54a0ff',
+  },
   headerContainer: {
+    borderBottomLeftRadius: 40,
+    zIndex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#2db7ff',
+    backgroundColor: '#54a0ff',
     paddingBottom: 20,
-    marginBottom: 10,
+  },
+  helperDesignView: {
+    position: 'absolute',
+    top: '15%',
+    height: '20%',
+    backgroundColor: 'white',
+    zIndex: 0,
+    width: '20%',
+  },
+  formContainer: {
+    backgroundColor: 'white',
+    borderRadius: 35,
+    width: '98%',
+    paddingBottom: 8,
+    marginBottom: 5,
   },
   iconCheck: {
     position: 'absolute',
@@ -417,7 +423,7 @@ const styles = StyleSheet.create({
     fontSize: 25,
   },
   cardTitle: {
-    marginTop: 10,
+    marginTop: 7,
     marginLeft: 15,
     marginBottom: 2,
     fontSize: 16,
@@ -448,5 +454,38 @@ const styles = StyleSheet.create({
   },
   touchableOpacityDatePicker: {
     width: '50%',
+  },
+  successMessage: {
+    alignItems: 'center',
+  },
+  successMessageIcon: {
+    position: 'absolute',
+    top: '35%',
+    left: '16%',
+    zIndex: 5,
+  },
+  successTextMessage: {
+    textAlign: 'center',
+    backgroundColor: '#4AB866',
+    alignItems: 'center',
+    padding: 8,
+    width: '85%',
+    borderRadius: 2,
+    marginTop: 8,
+  },
+  errorMessageIcon: {
+    position: 'absolute',
+    top: '36%',
+    left: '7%',
+    zIndex: 5,
+  },
+  errorTextMessage: {
+    textAlign: 'center',
+    backgroundColor: '#D94F4F',
+    alignItems: 'center',
+    padding: 8,
+    width: '89%',
+    borderRadius: 2,
+    marginTop: 8,
   },
 });
