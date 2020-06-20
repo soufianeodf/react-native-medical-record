7624030a11f71814e1b4253af70a26aa39174660
soufiane odf <soufiane.odf@gmail.com>
2020-06-20 21:37:03 +0100

    Add modal to medications list page

--- src/Components/Screens/ReferenceData.js
+++ src/Components/Screens/ReferenceData.js
@@ -8,6 +8,8 @@ import {
   TouchableOpacity,
   TextInput,
   Keyboard,
+  Modal,
+  Image,
 } from 'react-native';
 import firestore from '@react-native-firebase/firestore';
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -18,6 +20,7 @@ const ReferenceData = () => {
   const [loading, setLoading] = useState(true);
   const [medications, setMedications] = useState([{code: 1, nom: 'test'}]);
   const [isKeyboardOn, setIsKeyboardOn] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     Keyboard.addListener('keyboardDidShow', _keyboardDidShow);
@@ -77,10 +80,38 @@ const ReferenceData = () => {
         />
       </View>
       <View style={styles.buttonView}>
-        <TouchableOpacity onPress={() => Alert.alert('button clicked')}>
+        <TouchableOpacity onPress={() => setIsVisible(true)}>
           <Ionicons name={'ios-add-circle'} color={'orange'} size={66} />
         </TouchableOpacity>
       </View>
+
+      <Modal visible={isVisible} animationType="fade">
+        <View style={styles.modalContainerView}>
+          <View style={styles.modalFirstInnerView}>
+            <Image
+              style={styles.logoImage}
+              source={require('../../../images/medicine.png')}
+            />
+          </View>
+          <View style={styles.modalSecondInnerView}>
+            <View style={styles.modalViewMargin}>
+              <View style={styles.modalTitleView}>
+                <Text style={styles.modalTitleText}>Add New Medicine</Text>
+                <TouchableOpacity onPress={() => setIsVisible(false)}>
+                  <Ionicons name={'ios-close'} color={'grey'} size={35} />
+                </TouchableOpacity>
+              </View>
+              <Text style={styles.modalInputTextTitle}>Name</Text>
+              <TextInput style={styles.modalInputText} />
+              <TouchableOpacity
+                onPress={() => Alert.alert('adding...')}
+                style={styles.modalButtonContainer}>
+                <Text style={styles.modalButtonText}>Add Medicine</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -151,5 +182,62 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: '1.5%',
     right: '4%',
+  },
+  // Modal style
+  modalContainerView: {
+    flex: 1,
+    backgroundColor: 'orange',
+  },
+  modalFirstInnerView: {
+    flex: 2,
+    backgroundColor: 'orange',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  modalSecondInnerView: {
+    flex: 4,
+    backgroundColor: 'white',
+    borderTopRightRadius: 40,
+    borderTopLeftRadius: 40,
+    paddingTop: 40,
+  },
+  modalViewMargin: {
+    marginHorizontal: 25,
+  },
+  modalTitleView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  modalTitleText: {
+    fontWeight: 'bold',
+    fontSize: 17,
+    marginBottom: 50,
+  },
+  modalInputTextTitle: {
+    color: 'grey',
+  },
+  modalInputText: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#000',
+    marginHorizontal: 3,
+    marginBottom: 10,
+    padding: 3,
+  },
+  modalButtonContainer: {
+    borderWidth: 1,
+    backgroundColor: 'orange',
+    borderRadius: 3,
+    padding: 10,
+    borderColor: 'orange',
+    marginTop: 20,
+  },
+  logoImage: {
+    width: 176,
+    height: 176,
+  },
+  modalButtonText: {
+    textAlign: 'center',
+    color: 'white',
+    fontSize: 16,
   },
 });
