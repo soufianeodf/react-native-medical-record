8326287a547f7706aa6a12c3c7a7f774a4dca8e9
soufiane odf <soufiane.odf@gmail.com>
2020-06-23 19:04:34 +0100

    Add EventsList component

---
+++ src/Components/Screens/EventsList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+export default function EventsList() {
+  return (
+    <View>
+      <Text>Choose the type of the event you want to add</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
