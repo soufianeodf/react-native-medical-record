2afbc7458e532d021b0013f55d6ccc17b5778f21
soufiane odf <soufiane.odf@gmail.com>
2020-06-23 20:48:55 +0100

    Add navigation prop to eventList and update design

--- src/Components/Screens/EventsList.js
+++ src/Components/Screens/EventsList.js
@@ -1,13 +1,12 @@
 import React, {useEffect} from 'react';
-import {View, Text, StyleSheet, StatusBar} from 'react-native';
+import {View, Text, StyleSheet, StatusBar, TouchableOpacity} from 'react-native';
 import Fontisto from 'react-native-vector-icons/Fontisto';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import AntDesign from 'react-native-vector-icons/AntDesign';
-import {TouchableWithoutFeedback, TouchableOpacity} from 'react-native-gesture-handler';
+import {TouchableWithoutFeedback} from 'react-native-gesture-handler';
 
-export default function EventsList() {
+export default function EventsList({ navigation }) {
   useEffect(() => {
     StatusBar.setHidden(true);
   }, []);
@@ -17,7 +16,7 @@ export default function EventsList() {
       <Text style={{color: 'white', textAlign: 'center', marginTop: '-15%', marginBottom: 20, fontSize: 22}}>Choose the type of the event you want to add</Text>
       <View style={{flexWrap: 'wrap',flexDirection: 'row', justifyContent: 'space-around'}}>
         
-        <TouchableWithoutFeedback style={{alignItems: 'center', marginVertical: 20, marginRight: 10}} onPress={() => alert('clicked')}>
+        <TouchableWithoutFeedback style={{alignItems: 'center', marginVertical: 20, marginRight: 10}} onPress={() => navigation.navigate('DoctorAppointment')}>
           <View style={{padding: 16, backgroundColor: 'white', width: 67, hieght: 67, borderRadius: 1000, marginBottom: 10, alignItems: 'center'}}>
             <Fontisto name={'doctor'} color={'#fdc92e'} size={35} />
           </View>
@@ -56,8 +55,8 @@ export default function EventsList() {
 
       </View>
       <View style={styles.buttonView}>
-        <TouchableOpacity onPress={() => alert('clicked')}>
-          <AntDesign name={'closecircle'} color={'#1976d3'} size={50} />
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <AntDesign name={'closecircle'} color={'white'} size={50} />
         </TouchableOpacity>
       </View>
     </View>
