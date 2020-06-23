30eb35ee0b11574e9a02d9ed6fc22b8e7312f962
soufiane odf <soufiane.odf@gmail.com>
2020-06-23 21:04:31 +0100

    Externalize the style of eventList

--- src/Components/Screens/EventsList.js
+++ src/Components/Screens/EventsList.js
@@ -1,58 +1,82 @@
 import React, {useEffect} from 'react';
-import {View, Text, StyleSheet, StatusBar, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
 import Fontisto from 'react-native-vector-icons/Fontisto';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import {TouchableWithoutFeedback} from 'react-native-gesture-handler';
 
-export default function EventsList({ navigation }) {
+export default function EventsList({navigation}) {
   useEffect(() => {
     StatusBar.setHidden(true);
   }, []);
 
   return (
-    <View style={{flex: 1, backgroundColor: '#0b42a7', paddingHorizontal: 10, justifyContent: 'center'}}>
-      <Text style={{color: 'white', textAlign: 'center', marginTop: '-15%', marginBottom: 20, fontSize: 22}}>Choose the type of the event you want to add</Text>
-      <View style={{flexWrap: 'wrap',flexDirection: 'row', justifyContent: 'space-around'}}>
-        
-        <TouchableWithoutFeedback style={{alignItems: 'center', marginVertical: 20, marginRight: 10}} onPress={() => navigation.navigate('DoctorAppointment')}>
-          <View style={{padding: 16, backgroundColor: 'white', width: 67, hieght: 67, borderRadius: 1000, marginBottom: 10, alignItems: 'center'}}>
+    <View style={styles.viewContainer}>
+      <Text style={styles.titleText}>
+        Choose the type of the event you want to add
+      </Text>
+      <View style={styles.innerView}>
+        <TouchableWithoutFeedback
+          style={[styles.touchableWithoutFeedbackStyle, {marginRight: 10}]}
+          onPress={() => navigation.navigate('DoctorAppointment')}>
+          <View style={styles.iconView}>
             <Fontisto name={'doctor'} color={'#fdc92e'} size={35} />
           </View>
-          <Text style={{color: 'white'}}>DOCTOR</Text>
-          <Text style={{color: 'white'}}>APPOINTMENT</Text>
+          <Text style={styles.textSubTitle}>DOCTOR</Text>
+          <Text style={styles.textSubTitle}>APPOINTMENT</Text>
         </TouchableWithoutFeedback>
 
-        <TouchableWithoutFeedback style={{alignItems: 'center', marginVertical: 20}} onPress={() => alert('clicked')}>
-          <View style={{padding: 16, backgroundColor: 'white', width: 67, hieght: 67, borderRadius: 1000, marginBottom: 10, alignItems: 'center'}}>
+        <TouchableWithoutFeedback
+          style={styles.touchableWithoutFeedbackStyle}
+          onPress={() => alert('clicked')}>
+          <View style={styles.iconView}>
             <Fontisto name={'test-tube'} color={'#ef7650'} size={35} />
           </View>
-          <Text style={{color: 'white'}}>TEST</Text>
+          <Text style={styles.textSubTitle}>TEST</Text>
         </TouchableWithoutFeedback>
 
-        <TouchableWithoutFeedback style={{alignItems: 'center', marginVertical: 20}} onPress={() => alert('clicked')}>
-          <View style={{padding: 16, backgroundColor: 'white', width: 67, hieght: 67, borderRadius: 1000, marginBottom: 10, alignItems: 'center'}}>
-            <MaterialCommunityIcons name={'tape-measure'} color={'#ee6492'} size={35} />
+        <TouchableWithoutFeedback
+          style={styles.touchableWithoutFeedbackStyle}
+          onPress={() => alert('clicked')}>
+          <View style={styles.iconView}>
+            <MaterialCommunityIcons
+              name={'tape-measure'}
+              color={'#ee6492'}
+              size={35}
+            />
           </View>
-          <Text style={{color: 'white'}}>MEASUREMENT</Text>
+          <Text style={styles.textSubTitle}>MEASUREMENT</Text>
         </TouchableWithoutFeedback>
 
-        <TouchableWithoutFeedback style={{alignItems: 'center', marginVertical: 20}} onPress={() => alert('clicked')}>
-          <View style={{padding: 16, backgroundColor: 'white', width: 67, hieght: 67, borderRadius: 1000, marginBottom: 10, alignItems: 'center'}}>
-            <FontAwesome name={'thermometer-full'} color={'#7fdde9'} size={35} />
+        <TouchableWithoutFeedback
+          style={styles.touchableWithoutFeedbackStyle}
+          onPress={() => alert('clicked')}>
+          <View style={styles.iconView}>
+            <FontAwesome
+              name={'thermometer-full'}
+              color={'#7fdde9'}
+              size={35}
+            />
           </View>
-          <Text style={{color: 'white'}}>DISEASE</Text>
+          <Text style={styles.textSubTitle}>DISEASE</Text>
         </TouchableWithoutFeedback>
 
-        <TouchableWithoutFeedback style={{alignItems: 'center', marginVertical: 20}} onPress={() => alert('clicked')}>
-          <View style={{padding: 16, backgroundColor: 'white', width: 67, hieght: 67, borderRadius: 1000, marginBottom: 10, alignItems: 'center'}}>
+        <TouchableWithoutFeedback
+          style={styles.touchableWithoutFeedbackStyle}
+          onPress={() => alert('clicked')}>
+          <View style={styles.iconView}>
             <Fontisto name={'pills'} color={'#b388fe'} size={35} />
           </View>
-          <Text style={{color: 'white'}}>DRUGS</Text>
-          <Text style={{color: 'white'}}>RECEPTION</Text>
+          <Text style={styles.textSubTitle}>DRUGS</Text>
+          <Text style={styles.textSubTitle}>RECEPTION</Text>
         </TouchableWithoutFeedback>
-
       </View>
       <View style={styles.buttonView}>
         <TouchableOpacity onPress={() => navigation.goBack()}>
@@ -64,6 +88,40 @@ export default function EventsList({ navigation }) {
 }
 
 const styles = StyleSheet.create({
+  viewContainer: {
+    flex: 1,
+    backgroundColor: '#0b42a7',
+    paddingHorizontal: 10,
+    justifyContent: 'center',
+  },
+  titleText: {
+    color: 'white',
+    textAlign: 'center',
+    marginTop: '-15%',
+    marginBottom: 20,
+    fontSize: 22,
+  },
+  innerView: {
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  touchableWithoutFeedbackStyle: {
+    alignItems: 'center',
+    marginVertical: 20,
+  },
+  iconView: {
+    padding: 16,
+    backgroundColor: 'white',
+    width: 67,
+    borderRadius: 1000,
+    marginBottom: 10,
+    alignItems: 'center',
+  },
+
+  textSubTitle: {
+    color: 'white',
+  },
   buttonView: {
     position: 'absolute',
     bottom: '3%',
