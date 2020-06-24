711b1e58d017e19e2182880efb18b236e536cafb
soufiane odf <soufiane.odf@gmail.com>
2020-06-24 18:02:33 +0100

    Update the design of ReferenceDate component

--- src/Components/Screens/ReferenceData/index.js
+++ src/Components/Screens/ReferenceData/index.js
@@ -1,10 +1,157 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+  StatusBar,
+} from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import Fontisto from 'react-native-vector-icons/Fontisto';
 
-export default function index() {
+export default function index({navigation}) {
+  useEffect(() => {
+    StatusBar.setHidden(false);
+  }, []);
+
   return (
-    <View>
-      <Text>welcome to reference data</Text>
+    <View style={styles.viewContainer}>
+      <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+        <View style={styles.headerView}>
+          <TouchableOpacity
+            style={styles.iconView}
+            onPress={() => navigation.toggleDrawer()}>
+            <FontAwesome name={'navicon'} color={'grey'} size={25} />
+          </TouchableOpacity>
+          <View style={[styles.textView, {marginLeft: '25%'}]}>
+            <Text style={styles.textStyle}>User name</Text>
+          </View>
+        </View>
+
+        <TouchableOpacity
+          style={[styles.innerContainer, {backgroundColor: '#2c8bdb'}]}
+          onPress={() => alert('clicked')}>
+          <View style={styles.iconView}>
+            <Fontisto name={'doctor'} color={'white'} size={35} />
+          </View>
+          <View style={styles.textView}>
+            <Text style={styles.textStyle}>DOCTORS</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[styles.innerContainer, {backgroundColor: '#2f95ea'}]}
+          onPress={() => alert('clicked')}>
+          <View style={styles.iconView}>
+            <FontAwesome5 name={'book-medical'} color={'white'} size={35} />
+          </View>
+          <View style={styles.textView}>
+            <Text style={styles.textStyle}>MEDICATIONS DATABASE</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[styles.innerContainer, {backgroundColor: '#3ba1f6'}]}
+          onPress={() => alert('clicked')}>
+          <View style={styles.iconView}>
+            <FontAwesome5 name={'hospital-alt'} color={'white'} size={35} />
+          </View>
+          <View style={styles.textView}>
+            <Text style={styles.textStyle}>HOSPITALS</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[styles.innerContainer, {backgroundColor: '#46a7f8'}]}
+          onPress={() => alert('clicked')}>
+          <View style={styles.iconView}>
+            <FontAwesome5 name={'hospital-alt'} color={'white'} size={35} />
+          </View>
+          <View style={styles.textView}>
+            <Text style={styles.textStyle}>HOSPITALS</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[styles.innerContainer, {backgroundColor: '#55b2ff'}]}
+          onPress={() => alert('clicked')}>
+          <View style={styles.iconView}>
+            <FontAwesome5 name={'hospital-alt'} color={'white'} size={35} />
+          </View>
+          <View style={styles.textView}>
+            <Text style={styles.textStyle}>HOSPITALS</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[styles.innerContainer, {backgroundColor: '#6dbcff'}]}
+          onPress={() => alert('clicked')}>
+          <View style={styles.iconView}>
+            <FontAwesome5 name={'hospital-alt'} color={'white'} size={35} />
+          </View>
+          <View style={styles.textView}>
+            <Text style={styles.textStyle}>HOSPITALS</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[styles.innerContainer, {backgroundColor: '#81c6ff'}]}
+          onPress={() => alert('clicked')}>
+          <View style={styles.iconView}>
+            <FontAwesome5 name={'hospital-alt'} color={'white'} size={35} />
+          </View>
+          <View style={styles.textView}>
+            <Text style={styles.textStyle}>HOSPITALS</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[styles.innerContainer, {backgroundColor: '#9ad0ff'}]}
+          onPress={() => alert('clicked')}>
+          <View style={styles.iconView}>
+            <FontAwesome5 name={'hospital-alt'} color={'white'} size={35} />
+          </View>
+          <View style={styles.textView}>
+            <Text style={styles.textStyle}>HOSPITALS</Text>
+          </View>
+        </TouchableOpacity>
+      </ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  viewContainer: {
+    flex: 1,
+  },
+  scrollViewContainer: {
+    flexGrow: 1,
+    alignItems: 'center',
+  },
+  headerView: {
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    width: '100%',
+    paddingVertical: 10,
+  },
+  innerContainer: {
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    width: '100%',
+    paddingVertical: '4.8%',
+  },
+  iconView: {
+    marginLeft: 25,
+  },
+  textView: {
+    alignItems: 'center',
+    marginLeft: 15,
+    justifyContent: 'center',
+  },
+  textStyle: {
+    fontSize: 16,
+    fontFamily: 'serif',
+  },
+});
