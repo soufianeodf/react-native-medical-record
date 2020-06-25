9a44ef65aedf7f71db756a41bac1928b3eb1f9d7
soufiane odf <soufiane.odf@gmail.com>
2020-06-25 15:34:07 +0100

    Add medical act component

---
+++ src/Components/Screens/MedicalAct.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function MedicalAct() {
+  return (
+    <View>
+      <Text>medical act page</Text>
+    </View>
+  );
+}
