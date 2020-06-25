c94b69be8073e6a7fca889dcef291bfd0a24de36
soufiane odf <soufiane.odf@gmail.com>
2020-06-25 16:32:10 +0100

    Add views of MedicalAct

---
+++ src/Components/Screens/MedicalAct/MedicalPrescription.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function MedicalPrescription() {
+  return (
+    <View>
+      <Text>medical prescription</Text>
+    </View>
+  );
+}
