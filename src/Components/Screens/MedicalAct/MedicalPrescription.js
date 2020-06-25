645e5c5c9929c94e7686d6821491e91f856dbf12
soufiane odf <soufiane.odf@gmail.com>
2020-06-25 19:06:08 +0100

    Externalize the style

--- src/Components/Screens/MedicalAct/MedicalPrescription.js
+++ src/Components/Screens/MedicalAct/MedicalPrescription.js
@@ -1,43 +1,87 @@
 import React from 'react';
-import {View, Text, Image} from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import {View, Text, Image, ScrollView, StyleSheet} from 'react-native';
 
 export default function MedicalPrescription() {
   return (
-    <ScrollView style={{flex: 1, backgroundColor: '#fff'}}>
-      <View
-        style={{
-          flex: 1.2,
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          paddingLeft: '3%',
-          marginTop: '3%',
-        }}>
+    <ScrollView style={styles.viewContainer}>
+      <View style={styles.headerView}>
         <View>
-          <Text style={{fontSize: 25, fontWeight: 'bold'}}>Doctor Name</Text>
-          <Text style={{fontWeight: 'bold'}}>
+          <Text style={styles.headerTitleText}>Doctor Name</Text>
+          <Text style={styles.boldText}>
             Specialization : pneumo phtisiologie
           </Text>
-          <Text style={{fontWeight: 'bold'}}>
-            Address : Avenue des FAR immeuble
-          </Text>
-          <Text style={{fontWeight: 'bold'}}>Phone : +212 606 75 23 13</Text>
+          <Text style={styles.boldText}>Address : Avenue des FAR immeuble</Text>
+          <Text style={styles.boldText}>Phone : +212 606 75 23 13</Text>
         </View>
-        <View style={{justifyContent: 'center'}}>
+        <View style={styles.headerImageView}>
           <Image
             source={require('../../../../images/medicalhealthlogo.png')}
-            style={{width: 100, height: 100}}
+            style={styles.headerImage}
           />
         </View>
       </View>
-      <View style={{flex: 0.08, height: 7, backgroundColor: '#609AD0'}} />
-      <View style={{flex: 6, marginTop: '3%', paddingLeft: '3%', paddingRight: '10%'}}>
-        <Text style={{fontWeight: 'bold'}}>Date: 15/04/17</Text>
-        <Text style={{marginTop: '2%', marginBottom: '6%', fontWeight: 'bold'}}>Name: soufiane ouddaf</Text>
-        
-        <Text style={{fontWeight: 'bold', marginBottom: '1%', marginTop: '4%'}}>1) AVAMYS SPRAY NASAL :</Text>
-        <Text style={{lineHeight: 20, marginBottom: '3%'}}>2 PULVERISATION /NARINE /J LE MATIN X 15 JOURS PUIS 1 PULVERISATION /NARINE/J X 1 MOIS</Text>
+      <View style={styles.lineSeparatorView} />
+      <View style={styles.contentViewContainer}>
+        <Text style={styles.boldText}>Date: 15/04/17</Text>
+        <Text style={styles.nameTextStyle}>Name: soufiane ouddaf</Text>
+        <Text style={styles.titleDescriptionText}>1 AVAMYS SPRAY NASAL :</Text>
+        <Text style={styles.contentDescriptionText}>
+          Une desription de comment il faut prendre le medicament
+        </Text>
       </View>
     </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  viewContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  headerView: {
+    flex: 1.2,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingLeft: '3%',
+    marginTop: '3%',
+  },
+  headerTitleText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  headerImageView: {
+    justifyContent: 'center',
+  },
+  headerImage: {
+    width: 100,
+    height: 100,
+  },
+  lineSeparatorView: {
+    flex: 0.08,
+    height: 7,
+    backgroundColor: '#609AD0',
+  },
+  contentViewContainer: {
+    flex: 6,
+    marginTop: '3%',
+    paddingLeft: '3%',
+    paddingRight: '10%',
+  },
+  nameTextStyle: {
+    marginTop: '2%',
+    marginBottom: '6%',
+    fontWeight: 'bold',
+  },
+  titleDescriptionText: {
+    fontWeight: 'bold',
+    marginBottom: '1%',
+    marginTop: '4%',
+  },
+  contentDescriptionText: {
+    lineHeight: 20,
+    marginBottom: '3%',
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+});
