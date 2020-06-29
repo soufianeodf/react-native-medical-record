009ae0da5dfb7af8d072c01e8fb724eeae06ebef
soufiane odf <soufiane.odf@gmail.com>
2020-06-29 14:07:21 +0100

    Import and show the map

--- src/Components/Screens/JoinAnotherAccount.js
+++ src/Components/Screens/JoinAnotherAccount.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 const JoinAnotherAccount = () => {
   return (
-    <View>
-      <Text>welcome to JoinAnotherAccount page.</Text>
+    <View style={{flex: 1, justifyContent: 'flex-end'}}>
+      <MapView
+        style={{height: '90%', width: '100%'}}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   );
 };
