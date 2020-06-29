9e81ecda6e2c7fd5c408651727bf1bbe2d97b149
soufiane odf <soufiane.odf@gmail.com>
2020-06-29 17:03:40 +0100

    Show currentLocation

--- src/Components/Screens/JoinAnotherAccount.js
+++ src/Components/Screens/JoinAnotherAccount.js
@@ -1,19 +1,47 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, Text, View, Alert} from 'react-native';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
 
 const JoinAnotherAccount = () => {
+  const [initialPosition, setInitialPosition] = useState({
+    latitude: 31.5889,
+    longitude: -7.3626,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  useEffect(() => {
+    locateCurrentPosition();
+  }, []);
+
+  const locateCurrentPosition = () => {
+    Geolocation.getCurrentPosition(
+      position => {
+        console.log('------>', position);
+        let region = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        };
+        setInitialPosition(region);
+      },
+      error => console.error(error.message),
+      {
+        enableHighAccuracy: true,
+      },
+    );
+  };
+
   return (
     <View style={{flex: 1, justifyContent: 'flex-end'}}>
       <MapView
         style={{height: '90%', width: '100%'}}
         provider={PROVIDER_GOOGLE}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
+        showsUserLocation
+        followUserLocation
+        initialRegion={initialPosition}
       />
     </View>
   );
