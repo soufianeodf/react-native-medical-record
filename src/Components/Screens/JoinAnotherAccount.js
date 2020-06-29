e63fdb55057cdf795ccce42e5ffa5b73cd32f8c5
soufiane odf <soufiane.odf@gmail.com>
2020-06-29 21:25:57 +0100

    Show nearby hospitals by markers

--- src/Components/Screens/JoinAnotherAccount.js
+++ src/Components/Screens/JoinAnotherAccount.js
@@ -1,7 +1,8 @@
 import React, {useEffect, useState} from 'react';
-import {StyleSheet, Text, View, Alert} from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {StyleSheet, Alert} from 'react-native';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const JoinAnotherAccount = () => {
   const [initialPosition, setInitialPosition] = useState({
@@ -10,43 +11,89 @@ const JoinAnotherAccount = () => {
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   });
+  const [markers, setMarkers] = useState([]);
 
   useEffect(() => {
     locateCurrentPosition();
   }, []);
 
+  const nearbyHospitals = (latitude, longitude) => {
+    var places = [];
+    fetch(
+      'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' +
+        latitude +
+        ',' +
+        longitude +
+        '&radius=10000&type=hospital&key=AIzaSyBKufJtc7JQhJV_esN9vdubqz3VqMTFnHs',
+    )
+      .then(res => {
+        return res.json();
+      })
+      .then(res => {
+        for (let googlePlace of res.results) {
+          var place = {};
+          var myLat = googlePlace.geometry.location.lat;
+          var myLong = googlePlace.geometry.location.lng;
+          var coordinate = {
+            latitude: myLat,
+            longitude: myLong,
+          };
+          place.coordinate = coordinate;
+          place.placeName = googlePlace.name;
+          places.push(place);
+        }
+        setMarkers(places);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   const locateCurrentPosition = () => {
     Geolocation.getCurrentPosition(
       position => {
-        console.log('------>', position);
         let region = {
           latitude: position.coords.latitude,
           longitude: position.coords.longitude,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         };
+        nearbyHospitals(region.latitude, region.longitude);
         setInitialPosition(region);
       },
-      error => console.error(error.message),
-      {
-        enableHighAccuracy: true,
-      },
+      error => Alert.alert('Please activate your location.'),
+      {enableHighAccuracy: true, timeout: 20000},
     );
   };
 
   return (
-    <View style={{flex: 1, justifyContent: 'flex-end'}}>
-      <MapView
-        style={{height: '90%', width: '100%'}}
-        provider={PROVIDER_GOOGLE}
-        showsUserLocation
-        followUserLocation
-        initialRegion={initialPosition}
-      />
-    </View>
+    <MapView
+      style={styles.map}
+      provider={PROVIDER_GOOGLE}
+      showsUserLocation
+      followUserLocation
+      initialRegion={initialPosition}>
+      {markers.map(marker => (
+        <Marker
+          key={marker.coordinate.latitude}
+          coordinate={marker.coordinate}
+          title={marker.placeName}>
+          <MaterialCommunityIcons
+            name={'hospital-marker'}
+            color={'red'}
+            size={35}
+          />
+        </Marker>
+      ))}
+    </MapView>
   );
 };
 
 export default JoinAnotherAccount;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  map: {
+    height: '100%',
+    width: '100%',
+  },
+});
