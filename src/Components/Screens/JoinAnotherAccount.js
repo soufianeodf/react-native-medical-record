2dc1916d9126e3178758c0f6f3ce55999a99cbaf
soufiane odf <soufiane.odf@gmail.com>
2020-06-30 18:27:24 +0100

    Make JoinAnotherAccount page under construction

--- src/Components/Screens/JoinAnotherAccount.js
+++ src/Components/Screens/JoinAnotherAccount.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View, Image} from 'react-native';
 
 const JoinAnotherAccount = () => {
   return (
-    <View>
-      <Text>Welcome to JoinAnotherAccount page.</Text>
+    <View style={styles.viewContainer}>
+      <Image
+        source={require('../../../images/underConstruction.png')}
+        style={styles.imageStyle}
+      />
     </View>
   );
 };
 
 export default JoinAnotherAccount;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  viewContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#E6E6E6',
+  },
+  imageStyle: {
+    width: '100%',
+    height: '30%',
+  },
+});
