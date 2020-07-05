b9718dab71d8b084479b9643db5ceac5865a49db
soufiane odf <soufiane.odf@gmail.com>
2020-07-05 21:40:51 +0100

    Add ScrollView to MedicationCourse

--- src/Components/Screens/MedicationCourse.tsx
+++ src/Components/Screens/MedicationCourse.tsx
@@ -5,6 +5,7 @@ import {
   TextInput,
   TouchableOpacity,
   Keyboard,
+  ScrollView,
 } from 'react-native';
 import DateTimePickerModal from 'react-native-modal-datetime-picker';
 import moment from 'moment';
@@ -24,105 +25,107 @@ const MedicationCourse = ({navigation}) => {
     setDatePickerVisibility(false);
   };
 
-  const _handleConfirm = (date: string) => {
-    date = moment(date)
+  const _handleConfirm = (date: Date): void => {
+    const theDate = moment(date)
       .utc()
       .format('YYYY-MM-DD');
     _hideDatePicker();
-    setFromDate(date);
+    setFromDate(theDate);
   };
 
   return (
     <View style={{flex: 1, marginTop: 15}}>
-      <View style={styles.card}>
-        <View style={styles.cardContent}>
-          <TextInput
-            style={[styles.textInput, {width: '100%'}]}
-            onChangeText={theCourseTilte => setCourseTilte(theCourseTilte)}
-            placeholder={'Course title'}
-            value={courseTilte}
-            // onSubmitEditing={_handleUpdate}
-          />
-          <TextInput
-            style={[styles.textInput, {width: '100%'}]}
-            onTouchEnd={() => {
-              navigation.navigate('MedicationDatabase', {setMedication});
-              Keyboard.dismiss();
-            }}
-            placeholder={'Medication'}
-            value={medication}
-            // onSubmitEditing={_handleUpdate}
-          />
-
-          <TextInput
-            style={[styles.textInput, {width: '100%'}]}
-            placeholder={'Manufacturing form'}
-            // onTouchEnd={() => {
-            //   setIsGenderVisible(true);
-            //   Keyboard.dismiss();
-            // }}
-            // value={gender}
-            caretHidden={true}
-          />
-          <TextInput
-            style={styles.textInput}
-            onChangeText={theManufacturingForm =>
-              setManufacturingForm(theManufacturingForm)
-            }
-            placeholder={'Dosage'}
-            value={manufacturingForm}
-            keyboardType={'numeric'}
-          />
-          <TextInput
-            style={styles.textInput}
-            // onChangeText={thePhone => setPhone(thePhone)}
-            placeholder={'M. unit'}
-            keyboardType={'phone-pad'}
-            // value={phone}
-            // onSubmitEditing={_handleUpdate}
-          />
-        </View>
-      </View>
-
-      <View style={styles.card}>
-        <View style={styles.cardContent}>
-          <TouchableOpacity
-            style={styles.touchableOpacityDatePicker}
-            onPress={_showDatePicker}>
+      <ScrollView>
+        <View style={styles.card}>
+          <View style={styles.cardContent}>
             <TextInput
-              style={[styles.textInput, {width: '99%', color: '#000'}]}
-              editable={false}
-              placeholder={'From date'}
-              value={fromDate}
+              style={[styles.textInput, {width: '100%'}]}
+              onChangeText={theCourseTilte => setCourseTilte(theCourseTilte)}
+              placeholder={'Course title'}
+              value={courseTilte}
+              // onSubmitEditing={_handleUpdate}
             />
-          </TouchableOpacity>
-          <TextInput
-            style={[styles.textInput, {width: '100%'}]}
-            // onChangeText={fullName => setFullName_card_1(fullName)}
-            placeholder={'Course duration'}
-            // value={fullName_card_1}
-            // onSubmitEditing={_handleUpdate}
-          />
+            <TextInput
+              style={[styles.textInput, {width: '100%'}]}
+              onTouchEnd={() => {
+                navigation.navigate('MedicationDatabase', {setMedication});
+                Keyboard.dismiss();
+              }}
+              placeholder={'Medication'}
+              value={medication}
+              // onSubmitEditing={_handleUpdate}
+            />
 
-          <TextInput
-            style={[styles.textInput, {width: '100%'}]}
-            placeholder={'Taking interval'}
-            // onTouchEnd={() => {
-            //   setIsGenderVisible(true);
-            //   Keyboard.dismiss();
-            // }}
-            // value={gender}
-            caretHidden={true}
-          />
+            <TextInput
+              style={[styles.textInput, {width: '100%'}]}
+              placeholder={'Manufacturing form'}
+              // onTouchEnd={() => {
+              //   setIsGenderVisible(true);
+              //   Keyboard.dismiss();
+              // }}
+              // value={gender}
+              caretHidden={true}
+            />
+            <TextInput
+              style={styles.textInput}
+              onChangeText={theManufacturingForm =>
+                setManufacturingForm(theManufacturingForm)
+              }
+              placeholder={'Dosage'}
+              value={manufacturingForm}
+              keyboardType={'numeric'}
+            />
+            <TextInput
+              style={styles.textInput}
+              // onChangeText={thePhone => setPhone(thePhone)}
+              placeholder={'M. unit'}
+              keyboardType={'phone-pad'}
+              // value={phone}
+              // onSubmitEditing={_handleUpdate}
+            />
+          </View>
         </View>
-      </View>
 
-      <DateTimePickerModal
-        isVisible={isDatePickerVisible}
-        mode="date"
-        onConfirm={_handleConfirm}
-        onCancel={_hideDatePicker}
-      />
+        <View style={styles.card}>
+          <View style={styles.cardContent}>
+            <TouchableOpacity
+              style={styles.touchableOpacityDatePicker}
+              onPress={_showDatePicker}>
+              <TextInput
+                style={[styles.textInput, {width: '99%', color: '#000'}]}
+                editable={false}
+                placeholder={'From date'}
+                value={fromDate}
+              />
+            </TouchableOpacity>
+            <TextInput
+              style={[styles.textInput, {width: '100%'}]}
+              // onChangeText={fullName => setFullName_card_1(fullName)}
+              placeholder={'Course duration'}
+              // value={fullName_card_1}
+              // onSubmitEditing={_handleUpdate}
+            />
+
+            <TextInput
+              style={[styles.textInput, {width: '100%'}]}
+              placeholder={'Taking interval'}
+              // onTouchEnd={() => {
+              //   setIsGenderVisible(true);
+              //   Keyboard.dismiss();
+              // }}
+              // value={gender}
+              caretHidden={true}
+            />
+          </View>
+        </View>
+
+        <DateTimePickerModal
+          isVisible={isDatePickerVisible}
+          mode="date"
+          onConfirm={_handleConfirm}
+          onCancel={_hideDatePicker}
+        />
+      </ScrollView>
     </View>
   );
 };
@@ -140,6 +143,7 @@ const styles = StyleSheet.create({
     marginHorizontal: 15,
     marginBottom: 10,
     padding: 5,
+    marginTop: 2,
   },
   cardContent: {
     flexDirection: 'row',
