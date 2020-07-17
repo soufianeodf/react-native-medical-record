282aea359262d7f7f2204c87d76cd2c31542b6cd
soufiane odf <soufiane.odf@gmail.com>
2020-07-17 23:40:39 +0100

    Add button to create a family medical history

--- src/Components/Screens/Profile/FamilialPrecedents.tsx
+++ src/Components/Screens/Profile/FamilialPrecedents.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-const FamilialPrecedents = () => {
+const FamilialPrecedents = ({navigation}) => {
   return (
-    <View>
-      <Text>welcome to FamilialPrecedents page</Text>
+    <View style={{flex: 1}}>
+      <View>
+        <TextInput
+          onChange={(text) => console.log(text)}
+          value={'value'}
+          placeholder="value"
+        />
+      </View>
+      <View style={styles.buttonView}>
+        <TouchableOpacity onPress={() => navigation.navigate('EventsList')}>
+          <Ionicons name={'ios-add-circle'} color={'#3394ef'} size={66} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
-}
+};
 
 export default FamilialPrecedents;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  buttonView: {
+    position: 'absolute',
+    bottom: '1.5%',
+    right: '4%',
+  },
+});
