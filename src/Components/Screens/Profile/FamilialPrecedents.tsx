d03197ee5d579434d5a3763f34c83cc843c7d7e9
soufiane odf <soufiane.odf@gmail.com>
2020-07-18 00:32:34 +0100

    Remove unused code

--- src/Components/Screens/Profile/FamilialPrecedents.tsx
+++ src/Components/Screens/Profile/FamilialPrecedents.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  TextInput,
-} from 'react-native';
+import {StyleSheet, View, TouchableOpacity} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const FamilialPrecedents = ({navigation}) => {
   return (
     <View style={{flex: 1}}>
-      <View>
-        <TextInput
-          onChange={(text) => console.log(text)}
-          value={'value'}
-          placeholder="value"
-        />
-      </View>
       <View style={styles.buttonView}>
-        <TouchableOpacity onPress={() => navigation.navigate('EventsList')}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('AddFamilialPrecedents')}>
           <Ionicons name={'ios-add-circle'} color={'#3394ef'} size={66} />
         </TouchableOpacity>
       </View>
