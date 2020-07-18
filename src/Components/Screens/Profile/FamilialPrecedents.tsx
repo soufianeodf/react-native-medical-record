cdb328863beb0a2afd07c718d08a855e7d1a7a35
soufiane odf <soufiane.odf@gmail.com>
2020-07-18 02:00:36 +0100

    Add image and adapt view to portrait

--- src/Components/Screens/Profile/FamilialPrecedents.tsx
+++ src/Components/Screens/Profile/FamilialPrecedents.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
-import {StyleSheet, View, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View, TouchableOpacity, Image, Text} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const FamilialPrecedents = ({navigation}) => {
+  const [isPortrait, setIsPortrait] = useState(false);
+
+  const checkPortrait = (height, width) => {
+    setIsPortrait(height >= width);
+  };
+
   return (
     <View style={{flex: 1}}>
+      <View style={{flex: 1, alignItems: 'center', marginTop: isPortrait ? '20%' : 0}} onLayout={(event)=> checkPortrait(event.nativeEvent.layout.height, event.nativeEvent.layout.width)}>
+        <Image
+          style={styles.imageStyle}
+          source={require('../../../../images/addfamily.png')}
+        />
+        <Text style={{fontSize: 17, color: 'gray'}}>Update the patient's family medical history.</Text>
+      </View>
       <View style={styles.buttonView}>
         <TouchableOpacity
           onPress={() => navigation.navigate('AddFamilialPrecedents')}>
@@ -22,5 +35,9 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: '1.5%',
     right: '4%',
+  },
+  imageStyle: {
+    width: 300,
+    height: 250,
   },
 });
