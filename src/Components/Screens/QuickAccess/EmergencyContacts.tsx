d6b420a7f5f0bc126496e9239347c7ba627fbc27
soufiane odf <soufiane.odf@gmail.com>
2020-07-18 18:36:10 +0100

    Update UI of EmergencyContacts

--- src/Components/Screens/QuickAccess/EmergencyContacts.tsx
+++ src/Components/Screens/QuickAccess/EmergencyContacts.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-export default function EmergencyContacts() {
+export default function EmergencyContacts({navigation}) {
   return (
-    <View>
-      <Text>welcome to emergency contacts page.</Text>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Image
+        style={styles.imageStyle}
+        source={require('../../../../images/emergencycall.png')}
+      />
+      <Text style={{fontSize: 18, marginTop: 15}}>Emergency Contacts not Added</Text>
+      <View style={styles.buttonView}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('AddFamilialPrecedents')}>
+          <Ionicons name={'ios-add-circle'} color={'#661D54'} size={66} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  buttonView: {
+    position: 'absolute',
+    bottom: '1.5%',
+    right: '4%',
+  },
+  imageStyle: {
+    width: 150,
+    height: 150,
+  },
+});
