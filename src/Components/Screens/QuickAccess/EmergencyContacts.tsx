972f682d69dd06a7253c20ef13b36bdd44da7339
soufiane odf <soufiane.odf@gmail.com>
2020-07-18 23:30:38 +0100

    Fetch contacts from database

--- src/Components/Screens/QuickAccess/EmergencyContacts.tsx
+++ src/Components/Screens/QuickAccess/EmergencyContacts.tsx
@@ -1,37 +1,114 @@
-import React, {useState} from 'react';
-import {View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 
 const EmergencyContacts = ({navigation}) => {
   const [isPortrait, setIsPortrait] = useState(false);
+  const [contacts, setContacts] = useState([]);
 
-  return (
-    <View
-      style={[
-        styles.viewContainer,
-        {
-          marginTop: isPortrait ? '40%' : 0,
-          justifyContent: isPortrait ? 'flex-start' : 'center',
-        },
-      ]}
-      onLayout={(event) =>
-        setIsPortrait(
-          event.nativeEvent.layout.height >= event.nativeEvent.layout.width,
-        )
-      }>
-      <Image
-        style={styles.imageStyle}
-        source={require('../../../../images/emergencycall.png')}
-      />
-      <Text style={styles.textStyle}>Emergency Contacts not Added</Text>
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged((user) => {
+      if (user) {
+        getContacts(user.uid);
+      }
+    });
+    return subscriber;
+  }, []);
+
+  const getContacts = (uid) => {
+    let theContacts = [];
+    firestore()
+      .collection('emergencyContacts')
+      .doc(uid)
+      .collection('listContacts')
+      .get()
+      .then((querySnapshot) => {
+        console.log('Total contacts -----> : ', querySnapshot.size);
+        querySnapshot.forEach((documentSnapshot) => {
+          theContacts.push(documentSnapshot.data());
+        });
+        setContacts(theContacts);
+      })
+      .catch((error) => Alert.alert(error));
+  };
+
+  const _renderButtonView = () => {
+    return (
       <View style={styles.buttonView}>
         <TouchableOpacity
           onPress={() => navigation.navigate('AddEmergencyContact')}>
           <Ionicons name={'ios-add-circle'} color={'#661D54'} size={66} />
         </TouchableOpacity>
       </View>
-    </View>
-  );
+    );
+  };
+
+  const _renderWhenEmpty = () => {
+    return (
+      <View
+        style={[
+          styles.viewContainer,
+          {
+            marginTop: isPortrait ? '40%' : 0,
+            justifyContent: isPortrait ? 'flex-start' : 'center',
+          },
+        ]}
+        onLayout={(event) =>
+          setIsPortrait(
+            event.nativeEvent.layout.height >= event.nativeEvent.layout.width,
+          )
+        }>
+        <Image
+          style={styles.imageStyle}
+          source={require('../../../../images/emergencycall.png')}
+        />
+        <Text style={styles.textStyle}>Emergency Contacts not Added</Text>
+        {_renderButtonView()}
+      </View>
+    );
+  };
+
+  const _renderView = () => {
+    return (
+      <View style={{flex: 1, paddingHorizontal: 10}}>
+        {contacts.length !== 0
+          ? contacts.map((value) => {
+              return (
+                <TouchableOpacity
+                  onPress={() => alert('clicked')}
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    backgroundColor: 'white',
+                    marginTop: 10,
+                    padding: 8,
+                    borderRadius: 3,
+                    elevation: 3,
+                  }}>
+                  <Text style={{textAlignVertical: 'center'}}>name</Text>
+                  <Ionicons name={'ios-call'} size={30} />
+                  <Text style={{textAlignVertical: 'center'}}>
+                    phone number
+                  </Text>
+                </TouchableOpacity>
+              );
+            })
+          : _renderWhenEmpty()}
+        {_renderButtonView()}
+      </View>
+    );
+  };
+
+  return <>{_renderView()}</>;
 };
 
 export default EmergencyContacts;
