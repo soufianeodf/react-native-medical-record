bca0563159f8aaaa53b2e344ff3ffacd98bb7a4d
soufiane odf <soufiane.odf@gmail.com>
2020-07-18 19:51:01 +0100

    Externalize the style in AddEmergencyContact

--- src/Components/Screens/QuickAccess/AddEmergencyContact.tsx
+++ src/Components/Screens/QuickAccess/AddEmergencyContact.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
-import {View, Text, TextInput, TouchableOpacity} from 'react-native';
+import {Text, TextInput, TouchableOpacity, StyleSheet} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
 
 export default function AddEmergencyContact() {
   const [contactName, setContactName] = useState('');
@@ -7,16 +8,62 @@ export default function AddEmergencyContact() {
   const [city, setCity] = useState('');
 
   return (
-    <View style={{flex: 1, paddingHorizontal: 10, marginTop: '10%'}}>
-      <Text style={{fontSize: 17, marginBottom: -10, marginTop: 30}}>Contact Name</Text>
-      <TextInput style={{borderBottomWidth: 1, paddingBottom: 0}} onChange={(text) => setContactName(text)} value={contactName} placeholder='Enter the contact name here' />
-      <Text style={{fontSize: 17, marginBottom: -10, marginTop: 30}}>Phone</Text>
-      <TextInput style={{borderBottomWidth: 1, paddingBottom: 0}} onChange={(text) => setPhone(text)} value={phone} placeholder='Enter the phone number here' />
-      <Text style={{fontSize: 17, marginBottom: -10, marginTop: 30}}>City</Text>
-      <TextInput style={{borderBottomWidth: 1, paddingBottom: 0}} onChange={(text) => setCity(text)} value={city} placeholder='Enter the city name here' />
-      <TouchableOpacity onPress={() => alert('button pressed')} style={{backgroundColor: '#661D54', padding: 8, borderRadius: 2, marginTop: 30}} >
-        <Text style={{textAlign: 'center', fontSize: 17, color: '#ddd'}}>Add contact</Text>
+    <ScrollView style={styles.viewContainer}>
+      <Text style={styles.textStyle}>Contact Name</Text>
+      <TextInput
+        style={styles.textInputStyle}
+        onChange={(text) => setContactName(text)}
+        value={contactName}
+        placeholder="Enter the contact name here"
+      />
+      <Text style={styles.textStyle}>Phone</Text>
+      <TextInput
+        style={styles.textInputStyle}
+        onChange={(text) => setPhone(text)}
+        value={phone}
+        placeholder="Enter the phone number here"
+      />
+      <Text style={styles.textStyle}>City</Text>
+      <TextInput
+        style={styles.textInputStyle}
+        onChange={(text) => setCity(text)}
+        value={city}
+        placeholder="Enter the city name here"
+      />
+      <TouchableOpacity
+        onPress={() => alert('button pressed')}
+        style={styles.buttonStyle}>
+        <Text style={styles.textButton}>Add contact</Text>
       </TouchableOpacity>
-    </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  viewContainer: {
+    flex: 1,
+    paddingHorizontal: 10,
+    marginTop: '10%',
+  },
+  textStyle: {
+    fontSize: 17,
+    marginBottom: -10,
+    marginTop: 30,
+  },
+  textInputStyle: {
+    borderBottomWidth: 1,
+    paddingBottom: 0,
+  },
+  buttonStyle: {
+    backgroundColor: '#661D54',
+    padding: 8,
+    borderRadius: 2,
+    marginTop: 30,
+    marginBottom: 15,
+  },
+  textButton: {
+    textAlign: 'center',
+    fontSize: 17,
+    color: '#ddd',
+  },
+});
