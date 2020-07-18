511c278c00c3764b7d7498a9ce11afd2d9a54080
soufiane odf <soufiane.odf@gmail.com>
2020-07-18 14:46:30 +0100

    Add EmergencyContacts component

---
+++ src/Components/Screens/QuickAccess/EmergencyContacts.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function EmergencyContacts() {
+  return (
+    <View>
+      <Text>welcome to emergency contacts page.</Text>
+    </View>
+  );
+}
