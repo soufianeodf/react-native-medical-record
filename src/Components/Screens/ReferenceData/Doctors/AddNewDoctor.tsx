5d91600a4787455f1fb024bb21cd868fcc23f9b4
soufiane odf <soufiane.odf@gmail.com>
2020-07-19 20:34:35 +0100

    Add specilization and comment inputs

--- src/Components/Screens/ReferenceData/Doctors/AddNewDoctor.tsx
+++ src/Components/Screens/ReferenceData/Doctors/AddNewDoctor.tsx
@@ -61,11 +61,23 @@ const AddNewMedicine: React.FC<Props> = ({route, navigation}) => {
               <Ionicons name={'ios-close'} color={'grey'} size={35} />
             </TouchableOpacity>
           </View>
-          <Text style={styles.modalInputTextTitle}>Name</Text>
+          <Text style={styles.modalInputTextTitle}>Doctor's name</Text>
           <TextInput
             style={styles.modalInputText}
             value={fullName}
             onChangeText={(text) => setFullName(text)}
+          />
+          <Text style={styles.modalInputTextTitle}>Specialization</Text>
+          <TextInput
+            style={styles.modalInputText}
+            value={specialization}
+            onChangeText={(text) => setSpecialization(text)}
+          />
+          <Text style={styles.modalInputTextTitle}>Comment</Text>
+          <TextInput
+            style={styles.modalInputText}
+            value={comment}
+            onChangeText={(text) => setComment(text)}
           />
           <TouchableOpacity
             onPress={() => _addMedicine(route.params.uid)}
@@ -108,16 +120,17 @@ const styles = StyleSheet.create({
   modalTitleText: {
     fontWeight: 'bold',
     fontSize: 17,
-    marginBottom: 50,
+    marginBottom: 30,
   },
   modalInputTextTitle: {
     color: 'grey',
+    marginBottom: -5,
   },
   modalInputText: {
     borderBottomWidth: 1,
     borderBottomColor: 'grey',
     marginHorizontal: 3,
-    marginBottom: 10,
+    marginBottom: 20,
     padding: 3,
   },
   modalButtonContainer: {
@@ -126,7 +139,7 @@ const styles = StyleSheet.create({
     borderRadius: 3,
     padding: 10,
     borderColor: '#4B8DC9',
-    marginTop: 20,
+    marginTop: 10,
   },
   logoImage: {
     width: 200,
