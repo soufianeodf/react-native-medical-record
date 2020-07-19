bee568987f4609ba780a44e8c9dd3b3a4124ba25
soufiane odf <soufiane.odf@gmail.com>
2020-07-19 20:26:51 +0100

    Add doctor components

---
+++ src/Components/Screens/ReferenceData/Doctors/AddNewDoctor.tsx
@@ -0,0 +1,140 @@
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  Alert,
+} from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {RouteProp} from '@react-navigation/native';
+import {StackParamList} from '../../../../Navigation/Navigation';
+import {StackNavigationProp} from '@react-navigation/stack';
+
+type Props = {
+  route: RouteProp<StackParamList, 'AddNewMedicine'>;
+  navigation: StackNavigationProp<StackParamList, 'AddNewMedicine'>;
+};
+
+const AddNewMedicine: React.FC<Props> = ({route, navigation}) => {
+  const [fullName, setFullName] = useState('');
+  const [specialization, setSpecialization] = useState('');
+  const [comment, setComment] = useState('');
+
+  const _addMedicine = (theUid) => {
+    if (fullName !== '') {
+      firestore()
+        .collection('doctors')
+        .doc(theUid)
+        .collection('doctorlist')
+        .add({
+          fullName,
+          specialization,
+          comment,
+        })
+        .then(() => {
+          setFullName('');
+          route.params._setSelected();
+          navigation.goBack();
+        });
+    } else {
+      Alert.alert("You canno't enter an empty name.");
+    }
+  };
+
+  return (
+    <View style={styles.modalContainerView}>
+      <View style={styles.modalFirstInnerView}>
+        <Image
+          style={styles.logoImage}
+          source={require('../../../../../images/doctor.png')}
+        />
+      </View>
+      <View style={styles.modalSecondInnerView}>
+        <View style={styles.modalViewMargin}>
+          <View style={styles.modalTitleView}>
+            <Text style={styles.modalTitleText}>Add New Doctor</Text>
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+              <Ionicons name={'ios-close'} color={'grey'} size={35} />
+            </TouchableOpacity>
+          </View>
+          <Text style={styles.modalInputTextTitle}>Name</Text>
+          <TextInput
+            style={styles.modalInputText}
+            value={fullName}
+            onChangeText={(text) => setFullName(text)}
+          />
+          <TouchableOpacity
+            onPress={() => _addMedicine(route.params.uid)}
+            style={styles.modalButtonContainer}>
+            <Text style={styles.modalButtonText}>Add Doctor</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default AddNewMedicine;
+
+const styles = StyleSheet.create({
+  modalContainerView: {
+    flex: 1,
+    backgroundColor: '#4B8DC9',
+  },
+  modalFirstInnerView: {
+    flex: 2.5,
+    backgroundColor: '#4B8DC9',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  modalSecondInnerView: {
+    flex: 4,
+    backgroundColor: 'white',
+    borderTopRightRadius: 40,
+    borderTopLeftRadius: 40,
+    paddingTop: 30,
+  },
+  modalViewMargin: {
+    marginHorizontal: 25,
+  },
+  modalTitleView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  modalTitleText: {
+    fontWeight: 'bold',
+    fontSize: 17,
+    marginBottom: 50,
+  },
+  modalInputTextTitle: {
+    color: 'grey',
+  },
+  modalInputText: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'grey',
+    marginHorizontal: 3,
+    marginBottom: 10,
+    padding: 3,
+  },
+  modalButtonContainer: {
+    borderWidth: 1,
+    backgroundColor: '#4B8DC9',
+    borderRadius: 3,
+    padding: 10,
+    borderColor: '#4B8DC9',
+    marginTop: 20,
+  },
+  logoImage: {
+    width: 200,
+    height: 306.6,
+  },
+  modalButtonText: {
+    textAlign: 'center',
+    color: 'white',
+    fontSize: 16,
+  },
+});
---
+++ src/Components/Screens/ReferenceData/Doctors/Doctors.tsx
@@ -0,0 +1,259 @@
+import React, {useState, useEffect} from 'react';
+import {
+  StyleSheet,
+  Text,
+  Alert,
+  FlatList,
+  View,
+  TouchableOpacity,
+  TextInput,
+  Keyboard,
+  Image,
+} from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import Spinner from 'react-native-spinkit';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {StackParamList} from '../../../../Navigation/Navigation';
+import {RouteProp} from '@react-navigation/native';
+import auth from '@react-native-firebase/auth';
+
+type Props = {
+  route: RouteProp<StackParamList, 'MedicationDatabase'>;
+  navigation: StackNavigationProp<StackParamList, 'MedicationDatabase'>;
+};
+
+const Doctors: React.FC<Props> = ({navigation}) => {
+  const [loading, setLoading] = useState(true);
+  const [doctors, setDoctors] = useState([]);
+  const [isKeyboardOn, setIsKeyboardOn] = useState(false);
+  const [search, setSearch] = useState('');
+  const [selected, setSelected] = useState(false);
+  const [uid, setUid] = useState('');
+
+  useEffect(() => {
+    Keyboard.addListener('keyboardDidShow', _keyboardDidShow);
+    Keyboard.addListener('keyboardDidHide', _keyboardDidHide);
+    const subscriber = auth().onAuthStateChanged((user) => {
+      if (user) {
+        setUid(user.uid);
+        firestore()
+          .collection('doctors')
+          .doc(user.uid)
+          .collection('doctorlist')
+          .where('fullName', '>=', '')
+          .get()
+          .then((querySnapshot) => {
+            let theDoctors = [];
+            console.log('Total Doctors: ', querySnapshot.size);
+            querySnapshot.forEach((documentSnapshot) => {
+              theDoctors.push({
+                ...documentSnapshot.data(),
+                key: documentSnapshot.id,
+              });
+            });
+            setDoctors(theDoctors);
+            setLoading(false);
+          })
+          .catch((error) => Alert.alert(error));
+      }
+    });
+    return subscriber;
+  }, [selected]);
+
+  const _keyboardDidShow = () => {
+    setIsKeyboardOn(true);
+  };
+
+  const _keyboardDidHide = () => {
+    setIsKeyboardOn(false);
+  };
+
+  const _setSelected = () => {
+    setSelected(!selected);
+  };
+
+  const _search = () => {
+    firestore()
+      .collection('doctors')
+      .doc(uid)
+      .collection('doctorlist')
+      .where('fullName', '>=', '')
+      .get()
+      .then((querySnapshot) => {
+        let theDoctors = [];
+        console.log('Total Doctors ----------->: ', querySnapshot.size);
+        querySnapshot.forEach((documentSnapshot) => {
+          theDoctors.push({
+            ...documentSnapshot.data(),
+            key: documentSnapshot.id,
+          });
+        });
+        setDoctors(theDoctors);
+      })
+      .catch((error) => Alert.alert(error));
+  };
+
+  const _deleteSelectedMedicine = (item) => {
+    firestore()
+      .collection('doctors')
+      .doc(uid)
+      .collection('doctorlist')
+      .doc(item.key)
+      .delete()
+      .then(() => setSelected(!selected));
+  };
+
+  if (loading) {
+    return (
+      <View style={styles.activityIndicatorView}>
+        <Spinner isVisible={true} type={'Pulse'} color="#4B8DC9" size={70} />
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.viewContainer}>
+      <View style={[styles.firstInnerView, {flex: isKeyboardOn ? 1.9 : 0.8}]}>
+        <View style={styles.titleView}>
+          <Fontisto name={'doctor'} color={'white'} size={30} />
+          <Text style={styles.titleText}>Your Doctors</Text>
+        </View>
+        <View style={styles.searchBarView}>
+          <TouchableOpacity style={styles.searchBar} onPress={_search}>
+            <TextInput
+              style={styles.searchBarText}
+              onChangeText={(text) => setSearch(text)}
+              onSubmitEditing={_search}
+              placeholder="Type here..."
+            />
+            <Ionicons name={'md-search'} color={'grey'} size={30} />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.secondInnerView}>
+        {doctors.length === 0 ? (
+          <View style={styles.notFoundView}>
+            <Image
+              style={{
+                width: '100%',
+                height: isKeyboardOn ? '100%' : '75%',
+                borderRadius: 1000,
+              }}
+              source={require('../../../../../images/empty.png')}
+            />
+          </View>
+        ) : (
+          <FlatList
+            data={doctors}
+            extraData={selected}
+            renderItem={({item}) => (
+              <TouchableOpacity
+                onLongPress={() =>
+                  Alert.alert(
+                    'Delete chosen medication',
+                    'Are you sure to delete.',
+                    [
+                      {
+                        text: 'Cancel',
+                        style: 'cancel',
+                      },
+                      {
+                        text: 'OK',
+                        onPress: () => _deleteSelectedMedicine(item),
+                      },
+                    ],
+                    {cancelable: false},
+                  )
+                }>
+                <Text style={styles.itemsStyle}> {item.fullName} </Text>
+              </TouchableOpacity>
+            )}
+          />
+        )}
+      </View>
+      <View style={styles.buttonView}>
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate('AddNewDoctor', {_setSelected, uid})
+          }>
+          <Ionicons name={'ios-add-circle'} color={'#4B8DC9'} size={66} />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default Doctors;
+
+const styles = StyleSheet.create({
+  activityIndicatorView: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  viewContainer: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  firstInnerView: {
+    backgroundColor: '#4B8DC9',
+    borderBottomRightRadius: 25,
+    borderBottomLeftRadius: 25,
+    justifyContent: 'center',
+  },
+  titleView: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginVertical: 3,
+    paddingTop: 10,
+  },
+  titleText: {
+    color: 'white',
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 8,
+    marginLeft: 5,
+    fontFamily: 'serif',
+  },
+  searchBarView: {
+    alignItems: 'center',
+    marginBottom: 5,
+  },
+  searchBar: {
+    backgroundColor: 'white',
+    borderRadius: 50,
+    alignItems: 'center',
+    flexDirection: 'row',
+    paddingHorizontal: 4,
+  },
+  searchBarText: {
+    width: 320,
+    height: 40,
+    paddingLeft: 15,
+  },
+  secondInnerView: {
+    flex: 4,
+    backgroundColor: 'white',
+    padding: 5,
+    paddingTop: 10,
+  },
+  notFoundView: {
+    flex: 1,
+  },
+  itemsStyle: {
+    borderWidth: 1,
+    borderColor: 'white',
+    paddingVertical: 15,
+    margin: 8,
+    borderRadius: 3,
+    elevation: 1,
+    paddingLeft: 5,
+  },
+  buttonView: {
+    position: 'absolute',
+    bottom: '1.5%',
+    right: '4%',
+  },
+});
